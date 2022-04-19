e8aaf434cb6eebd4aa5a70a854dcc0ab799d67a5
nakata5321 <makar5321@gmail.com>
2022-04-19 13:22:48 +0300

    update front + add exception

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ class MainClass:
 
                     if self.current_command == "right_start":
                         logging.info(f"get command {self.current_command}")
-                        self.http.set_status_left("busy")
+                        self.http.set_status_right("busy")
 
                     if self.current_command == "left_stop":
                         logging.info(f"get command {self.current_command}")
@@ -84,18 +84,23 @@ class MainClass:
 
                     if self.current_command == "right_stop":
                         logging.info(f"get command {self.current_command}")
-                        self.http.set_status_left("available")
+                        self.http.set_status_right("available")
 
                     if self.current_command == "send_tokens":
-                        logging.info(f"get command {self.current_command}")
-                        result = self.interface.send_tokens(self.config["eisenkoch"]["address"],
-                                                            int(self.config["eisenkoch"]["cost"]))
-                        logging.info(result)
-                        account_data = self.interface.account_info(self.config["eisenkoch"]["address"])
-                        new_balance = round(
-                            (account_data["data"]["free"] - account_data["data"]["fee_frozen"]) * 10**(-9), 3)
-                        self.http.set_update_balance(new_balance)
-                        logging.info("update balance")
+                        try:
+                            logging.info(f"get command {self.current_command}")
+                            payment = self.config["eisenkoch"]["cost"] * 10**9
+                            result = self.interface.send_tokens(self.config["eisenkoch"]["address"], payment)
+                            logging.info(result)
+                            account_data = self.interface.account_info(self.config["eisenkoch"]["address"])
+                            new_balance = round(
+                                (account_data["data"]["free"] - account_data["data"]["fee_frozen"]) * 10**(-9), 3)
+                            self.http.set_update_balance(new_balance)
+                            logging.info("update balance")
+                        except Exception as e:
+                            logging.error("Not enough balance.")
+                            logging.error(e)
+                            pass
                         self.http.set_number_waffles(self.http.get_number_waffles() + 1)
 
                     if self.current_command == "left_timer_start":
